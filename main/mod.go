545e3ecc8b6752535a492594050c8561c9ace997
Артур <fazlyev.art@gmail.com>
2021-07-24 22:19:26 +0500

    initial commit

---
+++ main/mod.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	fmt.Scan(&i)
+	if i > 10000 {
+		fmt.Println("Число больше 10000, введите новое число")
+		fmt.Scan(&i)
+	} else if i < 0 {
+		fmt.Println("Число меньше 0, введите новое число")
+		fmt.Scan(&i)
+	} else {
+		fmt.Println(i % 100 / 10)
+	}
+
+}
